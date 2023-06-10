8dcdbb02cfd55bc561102e5e8a92fc722aa9eff8
Juan Andrés Quiroz <juanan0309@gmail.com>
2023-06-10 10:05:47 -0500

    Solve LeetCode 1802

---
+++ 1802.js
@@ -0,0 +1,18 @@
+var maxValue = function(n, index, maxSum) {
+    let answer = 1
+    let sum = n
+    let pointerA = index
+    let pointerB = index
+    while(sum < maxSum) {
+        if (pointerB - pointerA === n-1) {
+            answer+= Math.floor((maxSum - sum) / n )
+            break;
+        }
+        sum += pointerB - pointerA + 1
+        if (sum <= maxSum) answer++
+        if (pointerB < n-1) pointerB++
+        if (pointerA > 0) pointerA--
+    }
+    return answer
+
+};
